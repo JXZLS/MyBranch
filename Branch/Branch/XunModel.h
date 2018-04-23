b82fe30901d1ac3b2967e514942c5b2f728a5bd6
贾金勋 <jiajinxun@jiajinxundeMacBook-Pro.local>
2018-04-23 17:38:39 +0800

    xun

---
+++ Branch/Branch/XunModel.h
@@ -0,0 +1,16 @@
+//
+//  XunModel.h
+//  Branch
+//
+//  Created by 贾金勋 on 2018/4/23.
+//  Copyright © 2018年 JX. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XunModel : NSObject
+
+
+//  fjadsfjajlsfjaksj
+
+@end
---
+++ Branch/Branch/XunModel.m
@@ -0,0 +1,16 @@
+//
+//  XunModel.m
+//  Branch
+//
+//  Created by 贾金勋 on 2018/4/23.
+//  Copyright © 2018年 JX. All rights reserved.
+//
+
+#import "XunModel.h"
+
+@implementation XunModel
+
+
+
+
+@end
