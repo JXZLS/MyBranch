b95aa1ddb363693e2507f600c4bc1a9e7e20e28a
贾金勋 <jiajinxun@jiajinxundeMacBook-Pro.local>
2018-04-23 17:18:11 +0800

    jiatest

---
+++ Branch/Branch/JiaModel.h
@@ -0,0 +1,13 @@
+//
+//  JiaModel.h
+//  Branch
+//
+//  Created by 贾金勋 on 2018/4/23.
+//  Copyright © 2018年 JX. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JiaModel : NSObject
+
+@end
